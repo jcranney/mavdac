37e896e6570e15d5cbc8d1d38ed55af48aeda83a
Jesse Cranney <jesse.cranney@anu.edu.au>
2024-10-21 08:50:02 +1100

    ignore linting __init__

--- src/mavdac/__init__.py
+++ src/mavdac/__init__.py
@@ -1,10 +1,10 @@
-from .mavdac import *
+from .mavdac import *  # type: ignore # noqa: F403
 from .util import coeffs_from_cogs, run_mavdac
 
-__doc__ = mavdac.__doc__
+__doc__ = mavdac.__doc__  # type: ignore # noqa: F405
 __all__ = [
     "coeffs_from_cogs",
     "run_mavdac",
 ]
-if hasattr(mavdac, "__all__"):
-    __all__ += mavdac.__all__
+if hasattr(mavdac, "__all__"):  # type: ignore # noqa: F405
+    __all__ += mavdac.__all__  # type: ignore # noqa: F405
